0e2b7d170c417398f329e731c7c49d1c6742fa3a
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-02-28 23:37:26 +0200

    parseFloat

---
+++ script/scripts.js
@@ -0,0 +1,26 @@
+let x = prompt("number");
+let y = prompt("number");
+
+function sum(x, y) {
+    let z = parseFloat(x) + parseFloat(y)
+    alert(`Результат: ${x} + ${y}=${z}`)
+}
+sum(x, y)
+
+function subtraction(x, y) {
+    let z = x - y
+    alert(`Результат: ${x} - ${y}=${z}`)
+}
+subtraction(x, y)
+
+function multiplication(x, y) {
+    let z = x * y
+    alert(`Результат: ${x} * ${y}=${z}`)
+}
+multiplication(x, y)
+
+function division(x, y) {
+    let z = x / y
+    alert(`Результат: ${x} / ${y}=${z}`)
+}
+division(x, y)
