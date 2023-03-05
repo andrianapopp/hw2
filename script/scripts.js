f47e6b61498d3bd4eb8da42907d178acecd19e4e
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-03-05 12:21:28 +0200

    function const

--- script/scripts.js
+++ script/scripts.js
@@ -2,25 +2,25 @@ const x = prompt("number");
 const y = prompt("number");
 
 function sum(x, y) {
-    let z = parseFloat(x) + parseFloat(y)
+    const z = parseFloat(x) + parseFloat(y)
     alert(`Результат: ${x} + ${y}=${z}`)
 }
 sum(x, y)
 
 function subtraction(x, y) {
-    let z = x - y
+    const z = x - y
     alert(`Результат: ${x} - ${y}=${z}`)
 }
 subtraction(x, y)
 
 function multiplication(x, y) {
-    let z = x * y
+    const z = x * y
     alert(`Результат: ${x} * ${y}=${z}`)
 }
 multiplication(x, y)
 
 function division(x, y) {
-    let z = x / y
+    const z = x / y
     alert(`Результат: ${x} / ${y}=${z}`)
 }
 division(x, y)
